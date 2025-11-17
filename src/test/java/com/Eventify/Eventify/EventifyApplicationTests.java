d21b77cb7217f5e924f845dc1090e3cd8e37f230
Hamza <echchablihamza1@gmail.com>
2025-11-17 11:24:55 -0800

    first commit

---
+++ src/test/java/com/Eventify/Eventify/EventifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eventify.Eventify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
