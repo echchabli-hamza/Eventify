e47f084ec76be78718f1caac2f8eba6ab33f4498
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-11-19 11:20:32 +0100

    Create custom exceptions to handle errors

---
+++ src/main/java/com/Eventify/Eventify/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.Eventify.Eventify.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
